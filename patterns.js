52d2b343425da7f80a47e56c7993904126f38ca0
katerinaSo <katya.solstad@gmail.com>
2019-05-28 19:17:14 -0400

    countingUnique function added for sorted array

--- patterns.js
+++ patterns.js
@@ -57,4 +57,19 @@ const validAnagram = (str1, str2) => {
 validAnagram("rtyu", "utyr");
 
 
+// getting array of uniques from sorted array using object
+
+function countingUniques(arr){
+    let i=0;
+    if(arr.length===0)return 0;
+    for(let j=1;j<arr.length;j++){
+        if(arr[i]!==arr[j]){
+            i++;
+            arr[i]=arr[j]
+        }
+
+    }
+    return i+1
+}
+
 
