b46d4f22724487de53ffe49f23bf2fd9f3e41ace
katerinaSo <katya.solstad@gmail.com>
2019-06-18 09:09:48 -0400

    divide and conquer added

--- DivideAndConquer.js
+++ DivideAndConquer.js
@@ -62,7 +62,7 @@ function areThereDuplicates2(...args) {
   args.sort((a, b) => a > b);
   let start = 0;
   let next = 1;
-  while (next < atgs.length) {
+  while (next < args.length) {
     if (args[start] === args[next]) {
       return true;
     }
@@ -74,4 +74,41 @@ function areThereDuplicates2(...args) {
 
 function areThereDuplicates3() {
   return new Set(arguments).size !== arguments.length;
+}
+
+// SORTED array and num that represents if there is an average in arr matches that num
+
+function averagePair(arr, num) {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start < end) {
+    let avg = (arr[start] + arr[end]) / 2;
+    if (avg === num) return true;
+    else if (avg < num) start++;
+    else end--;
+  }
+  return false;
+}
+
+// isSubsequence function to see if characters of first string exist and appear in same order into second string
+// time complexity O(N+M)
+// space complexity (O(1))
+function isSubsequence(str1, str2) {
+  let i = 0;
+  let j = 0;
+  if (!str2) return true;
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) i++;
+    if (i === str1.length) return true;
+    j++;
+  }
+  return false;
+}
+
+// recursive solution (space complexity is more than O(1))
+function isSubsequence(str1, str2) {
+  if (str1.length === 0) return true;
+  if (str2.length === 0) return false;
+  if (str1[0] === str2[0]) return isSubsequence(str1.slice(1), str2.slice(1));
+  return isSubsequence(srt1, srt2.slice(1));
 }
